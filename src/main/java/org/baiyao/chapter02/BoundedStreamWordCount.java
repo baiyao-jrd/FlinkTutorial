5ace1b7b4521a0b6c013ac12d1fc21c0b7f390bd
江润冬 <83505019+baiyao-jrd@users.noreply.github.com>
2022-08-19 01:10:02 +0800

    单词计数流处理案例

--- src/main/java/org/baiyao/chapter02/BoundedStreamWordCount.java
+++ src/main/java/org/baiyao/chapter02/BoundedStreamWordCount.java
@@ -1,7 +1,32 @@
 package org.baiyao.chapter02;
 
-public class BoundedStreamWordCount {
-    public static void main(String[] args) {
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
+public class BoundedStreamWordCount {
+    public static void main(String[] args) throws Exception{
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        env
+                .readTextFile("input/words.txt")
+                .flatMap(new FlatMapFunction<String, Tuple2<String, Long>>() {
+                    public void flatMap(String in, Collector<Tuple2<String, Long>> out) throws Exception {
+                        String[] words = in.split(" ");
+                        for (String word : words) {
+                            out.collect(Tuple2.of(word, 1L));
+                        }
+                    }
+                })
+                .keyBy(new KeySelector<Tuple2<String, Long>, String>() {
+                    public String getKey(Tuple2<String, Long> in) throws Exception {
+                        return in.f0;
+                    }
+                })
+                .sum(1)
+                .print();
+        env.execute();
     }
 }
