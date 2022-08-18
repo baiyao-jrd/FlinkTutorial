2430515a3e8f134de445e4c477d4de7c2136a81d
baiyao-jrd <baiyao.org@gmail.com>
2022-08-19 01:21:49 +0800

    单词计数无界流处理案例

--- src/main/java/org/baiyao/chapter02/StreamWordCount.java
+++ src/main/java/org/baiyao/chapter02/StreamWordCount.java
@@ -6,12 +6,12 @@ import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
-public class BoundedStreamWordCount {
+public class StreamWordCount {
     public static void main(String[] args) throws Exception{
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 //        env.setParallelism(1);
         env
-                .readTextFile("input/words.txt")
+                .socketTextStream("hadoop102", 8888)
                 .flatMap(new FlatMapFunction<String, Tuple2<String, Long>>() {
                     public void flatMap(String in, Collector<Tuple2<String, Long>> out) throws Exception {
                         String[] words = in.split(" ");
